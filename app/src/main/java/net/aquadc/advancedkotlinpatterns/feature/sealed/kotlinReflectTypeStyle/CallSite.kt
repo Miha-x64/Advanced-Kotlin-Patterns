45a7743e7a3405c728ad45e2f6bbfac387016e03
Miha_x64 <mihlo1996@rambler.ru>
2017-09-05 21:59:02 +0300

    println() in sealed Type call-site

--- app/src/main/java/net/aquadc/advancedkotlinpatterns/feature/sealed/kotlinReflectTypeStyle/CallSite.kt
+++ app/src/main/java/net/aquadc/advancedkotlinpatterns/feature/sealed/kotlinReflectTypeStyle/CallSite.kt
@@ -2,13 +2,13 @@ package net.aquadc.advancedkotlinpatterns.feature.sealed.kotlinReflectTypeStyle
 
 fun main(args: Array<String>) {
     val type = getTypeSomewhere()
-    when (type) {
-        is Type.Class -> { }
-        is Type.ParameterizedType -> { }
-        is Type.GenericArrayType -> { }
-        is Type.TypeVariable<*> -> { }
-        is Type.WildcardType -> { }
-    }!!
+    println(when (type) {
+        is Type.Class -> "class"
+        is Type.ParameterizedType -> "parameterized type"
+        is Type.GenericArrayType -> "generic array type"
+        is Type.TypeVariable<*> -> "type variable"
+        is Type.WildcardType -> "wildcard type"
+    })
 }
 
 private fun getTypeSomewhere(): Type = throw UnsupportedOperationException()
