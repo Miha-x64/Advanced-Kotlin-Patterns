22d3e24057ecd3e13e8b7234fac017c1549c7807
Miha_x64 <mihlo1996@rambler.ru>
2017-09-08 17:06:08 +0300

    Shoot your colleague's leg implementing Type

---
+++ app/src/main/java/net/aquadc/advancedkotlinpatterns/feature/sealed/shootALegWithJavaReflectType/Shoot.java
@@ -0,0 +1,11 @@
+package net.aquadc.advancedkotlinpatterns.feature.sealed.shootALegWithJavaReflectType;
+
+import java.lang.reflect.Type;
+
+public class Shoot {
+
+    public Type getTypeSomewhere() {
+        return new Type() {};
+    }
+
+}
